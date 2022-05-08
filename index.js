c882627b6423025ba52bee5f6a2be86c648406f4
dexter1905 <debadityasen17@gmail.com>
2022-05-08 18:04:17 +0530

    First Commit : Project Setup

---
+++ index.js
@@ -0,0 +1,21 @@
+var randomNumber1 = Math.random() * 6;
+randomNumber1 = Math.floor(randomNumber1) + 1;
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomImageSource1 = "images/" + randomDiceImage1;
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomImageSource1);
+
+var randomNumber2 = Math.random() * 6;
+randomNumber2 = Math.floor(randomNumber2) + 1;
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+var randomImageSource2 = "images/" + randomDiceImage2;
+var image2 = document.querySelectorAll("img")[1];
+image2.setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏴 Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏳 Player 2 wins";
+} else {
+    document.querySelector("h1").innerHTML = "It is a draw";
+}
